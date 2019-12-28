33b9d0d88290263f12a7bec70c5b37c83728aab6
ahaenggli <adriano@haenggli.net>
2019-12-28 21:46:23 +0100

    fix unsafeprotectedbroadcastreceiver

--- app/src/main/java/ch/trachtengruppe_merenschwand/mytrachtenapp/MyBroadcastReceiver.java
+++ app/src/main/java/ch/trachtengruppe_merenschwand/mytrachtenapp/MyBroadcastReceiver.java
@@ -13,34 +13,45 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        boolean Benachrichtigung = false;
-        boolean Ton = false;
-        boolean Vibration = false;
-        boolean Licht = false;
+    switch (intent.getAction()) {
+        case Intent.ACTION_BOOT_COMPLETED:
 
+            boolean Benachrichtigung = false;
+            boolean Ton = false;
+            boolean Vibration = false;
+            boolean Licht = false;
 
-        SharedPreferences settings;
-        settings = context.getSharedPreferences(context.getString(R.string.app_settings), context.MODE_PRIVATE);
+            SharedPreferences settings;
+            settings = context.getSharedPreferences(context.getString(R.string.app_settings), context.MODE_PRIVATE);
 
-        if(settings != null)
-        {
+            if(settings != null)
+            {
 
-            Benachrichtigung = settings.getBoolean(context.getString(R.string.app_settings_benachrichtigung), true);
-            Ton = settings.getBoolean(context.getString(R.string.app_settings_ton), true);
-            Vibration = settings.getBoolean(context.getString(R.string.app_settings_vibration), false);
-            Licht = settings.getBoolean(context.getString(R.string.app_settings_licht), true);
+                Benachrichtigung = settings.getBoolean(context.getString(R.string.app_settings_benachrichtigung), true);
+                Ton = settings.getBoolean(context.getString(R.string.app_settings_ton), true);
+                Vibration = settings.getBoolean(context.getString(R.string.app_settings_vibration), false);
+                Licht = settings.getBoolean(context.getString(R.string.app_settings_licht), true);
 
-            if(Benachrichtigung){
-                context.stopService(new Intent(context, RssService.class));
-                context.startService(new Intent(context, RssService.class)
-                                .putExtra("Ton", Ton)
-                                .putExtra("Vibration", Vibration)
-                                .putExtra("Licht", Licht)
-                );
+                if(Benachrichtigung){
+                    context.stopService(new Intent(context, RssService.class));
+                    context.startService(new Intent(context, RssService.class)
+                            .putExtra("Ton", Ton)
+                            .putExtra("Vibration", Vibration)
+                            .putExtra("Licht", Licht)
+                    );
+                }
+                else    context.stopService(new Intent(context, RssService.class));
+
             }
-            else    context.stopService(new Intent(context, RssService.class));
 
-        }
+        break;
+        default:
+        break;
+
+    }
+
+
+
 
     }
 }
